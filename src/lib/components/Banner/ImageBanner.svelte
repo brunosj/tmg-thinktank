d33883cdc30752496c9ce520814e84842679af4d
brunosj <contact@landozone.net>
2024-01-10 10:27:07 +0100

    image banner

---
+++ src/lib/components/Banner/ImageBanner.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	export let items: PublicationFeatureType[];
+
+	import type { PublicationFeature as PublicationFeatureType } from '$lib/types/types';
+
+	let featuredItem: PublicationFeatureType | undefined;
+
+	featuredItem = items.filter((item) => item.fields.featuredOnHomepage === true)[0];
+</script>
+
+{#if featuredItem}
+	<section class="h-full w-full">
+		<div class="justify-center">
+			<div class="z-0 m-auto">
+				<img
+					loading="lazy"
+					src={featuredItem.fields.pageBannerCdn[0].secure_url}
+					alt={featuredItem.fields.title}
+					class="h-full w-full object-cover lg:h-[60vh]"
+				/>
+			</div>
+		</div>
+	</section>
+{/if}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,44 @@
-<script>
+<script lang="ts">
+	export let data: Page;
+
+	import type {
+		LandingPage,
+		Programme,
+		Newsletter,
+		Partner,
+		Event,
+		News,
+		PublicationFeature
+	} from '$lib/types/types';
 	import HeroV2 from '$components/Hero/HeroV2.svelte';
 	import Hero from '$components/Hero/Hero.svelte';
 	import HeroProgrammesV2 from '$components/Hero/HeroProgrammesV2.svelte';
 	import LatestV2 from '$components/Latest/LatestV2.svelte';
 	import NewsletterBanner from '$components/Banner/NewsletterBanner.svelte';
 	import NetworksBanner from '$components/Banner/NetworksBanner.svelte';
-	export let data;
 	import BlogHomepage from '$components/Blog/BlogHomepage.svelte';
 	import EventsHomepage from '$components/Events/EventsHomepage.svelte';
+	import ImageBanner from '$components/Banner/ImageBanner.svelte';
+
+	type Page = {
+		landingPage: LandingPage[];
+		programmes: Programme[];
+		newsletter: Newsletter[];
+		partners: Partner[];
+		events: Event[];
+		news: News[];
+		publicationFeatures: PublicationFeature[];
+	};
 
 	let landingPage = data.landingPage?.[0].fields;
-	let { programmes, newsletter, partners, events, news } = data;
+	let { programmes, newsletter, partners, events, news, publicationFeatures } = data;
 </script>
 
-<Hero hero={landingPage} heroProgrammes={programmes} />
+<Hero hero={landingPage} />
 <!-- <HeroV2 hero={landingPage} heroProgrammes={programmes} /> -->
 <LatestV2 {landingPage} />
 <EventsHomepage {events} {landingPage} />
+<ImageBanner items={publicationFeatures} />
 <HeroProgrammesV2 heroProgrammes={programmes} />
 <BlogHomepage {news} {landingPage} />
 <NewsletterBanner {newsletter} {landingPage} />
