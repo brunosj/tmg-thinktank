d20ca6280448c18989f7b7d00add7d6706d17685
brunosj <contact@landozone.net>
2024-06-18 10:50:55 +0200

    video news page redesign

--- src/lib/components/Video/VideoListing.svelte
+++ src/lib/components/Video/VideoListing.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	export let videos: Video[] | Video;
+	export let showTitle: boolean = true;
 
 	import type { Video } from '$lib/types/types';
 	import VideoWrapper from '$components/Video/VideoWrapper.svelte';
@@ -7,62 +8,62 @@
 	$: videosArray = Array.isArray(videos) ? videos : [videos];
 </script>
 
-<div class="bg-green-light">
-	<ul class="">
-		<div class="">
-			{#if videosArray.length >= 1}
-				<ul class="grid grid-cols-1 gap-6 pb-6 pt-6 md:grid-cols-2 lg:pb-12 lg:pt-12">
-					{#each videosArray as video, i (video.fields.url)}
-						{@const image =
-							video.fields.imageCdn?.length > 0
-								? video.fields.imageCdn[0].secure_url
-								: video.fields.image.fields.file.url}
-						<li class="pb-5" id={video.fields.videoId}>
-							<div class="h-full border">
-								<VideoWrapper
-									videoSrcURL={video.fields.url}
-									videoWidth="600"
-									videoHeight="337"
-									videoTitle={video.fields.title}
-									videoImage={image}
-								/>
-								<div class="p-4">
-									<h1
-										class="font pt-3 text-base font-bold leading-tight text-black group-hover:text-green-normal lg:text-xl"
-									>
-										{video.fields.title}
-									</h1>
-									<p class="pb-2 pt-4 text-sm text-black">
-										<span>{video.fields.summary}</span>
-									</p>
-								</div>
-							</div>
-						</li>
-					{/each}
-				</ul>
-			{:else}
-				<ul class=" h-[full]">
-					<li>
+<div class="">
+	{#if videosArray.length > 1}
+		<ul class="grid grid-cols-1 gap-6 py-6 md:grid-cols-2">
+			{#each videosArray as video, i (video.fields.url)}
+				{@const image =
+					video.fields.imageCdn?.length > 0
+						? video.fields.imageCdn[0].secure_url
+						: video.fields.image.fields.file.url}
+				<li class="pb-5" id={video.fields.videoId}>
+					<div class="h-full border">
 						<VideoWrapper
-							videoSrcURL={videosArray[0].fields.url}
-							videoWidth="1248"
-							videoHeight="600"
-							videoTitle={videosArray[0].fields.title}
-							videoImage={videosArray[0].fields.image.fields.file.url}
+							videoSrcURL={video.fields.url}
+							videoWidth="600"
+							videoHeight="337"
+							videoTitle={video.fields.title}
+							videoImage={image}
 						/>
-						<div class="p-4">
-							<h1
-								class="font pt-3 text-base font-bold leading-tight text-black group-hover:text-green-normal lg:text-xl"
-							>
-								{videosArray[0].fields.title}
-							</h1>
-							<p class="pb-2 pt-4 text-sm text-black">
-								<span>{videosArray[0].fields.summary}</span>
-							</p>
-						</div>
-					</li>
-				</ul>
-			{/if}
-		</div>
-	</ul>
+						{#if showTitle}
+							<div class="p-4">
+								<h1
+									class="font pt-3 text-base font-bold leading-tight text-black group-hover:text-green-normal lg:text-xl"
+								>
+									{video.fields.title}
+								</h1>
+								<p class="pb-2 pt-4 text-sm text-black">
+									<span>{video.fields.summary}</span>
+								</p>
+							</div>
+						{/if}
+					</div>
+				</li>
+			{/each}
+		</ul>
+	{:else}
+		<ul class="h-[full]">
+			<li>
+				<VideoWrapper
+					videoSrcURL={videosArray[0].fields.url}
+					videoWidth="1248"
+					videoHeight="600"
+					videoTitle={videosArray[0].fields.title}
+					videoImage={videosArray[0].fields.image.fields.file.url}
+				/>
+				{#if showTitle}
+					<div class="p-4">
+						<h1
+							class="font pt-3 text-base font-bold leading-tight text-black group-hover:text-green-normal lg:text-xl"
+						>
+							{videosArray[0].fields.title}
+						</h1>
+						<p class="pb-2 pt-4 text-sm text-black">
+							<span>{videosArray[0].fields.summary}</span>
+						</p>
+					</div>
+				{/if}
+			</li>
+		</ul>
+	{/if}
 </div>
--- src/routes/video/[slug]/+page.svelte
+++ src/routes/video/[slug]/+page.svelte
@@ -23,39 +23,40 @@
 </script>
 
 <SEO title={item.fields.title} description={item.fields.summary} {image} />
-<article class="container overflow-hidden py-24 lg:py-32">
-	<section class="space-y-7 border-b border-green-normal pb-6 lg:pb-12">
+<article class="overflow-hidden pt-16 lg:pt-32">
+	<section class="container space-y-6 border-b border-green-normal pb-6 lg:pb-12">
 		<h3 class="font-semibold leading-relaxed text-black">{item.fields.type}</h3>
 		<h1 class="font-bold leading-tight text-green-normal">
 			{item.fields.title}
 		</h1>
 		<p class="text-xl">{item.fields.summary}</p>
 	</section>
-
-	<section class="richText w-full py-6 lg:w-2/3 lg:py-12">
-		{@html renderRichText(item.fields.descriptionRich)}
+	<section class="m-auto w-full bg-green-normal">
+		<div class="container m-auto py-6 lg:w-2/3">
+			<VideoListing videos={item.fields.video || []} showTitle={false} />
+		</div>
 	</section>
-	<VideoListing videos={item.fields.video || []} />
 
-	{#if item.fields.relatedNews.length > 0}
+	<section class="richText container w-full py-6 lg:w-2/3 lg:py-12">
+		{@html renderRichText(item.fields.descriptionRich)}
+		{#if item.fields.relatedNews.length > 0}
+			<div class="pt-6">
+				<div class="text-xl font-semibold lg:text-2xl">Related News</div>
+				<NewsListing items={item.fields.relatedNews} />
+			</div>
+		{/if}
+
+		{#if item.fields.relatedPublications.length > 0}
+			<div class="pt-6">
+				<div class="text-xl font-semibold lg:text-2xl">Related Publications</div>
+				<PublicationListing items={item.fields.relatedPublications} />
+			</div>
+		{/if}
+
 		<div class="pt-6">
-			<div class="text-xl font-semibold lg:text-2xl">Related News</div>
-			<NewsListing items={item.fields.relatedNews} />
+			<Button colors="green" to="/videos">View All Videos</Button>
 		</div>
-	{/if}
-
-	{#if item.fields.relatedPublications.length > 0}
-		<div class="pt-6">
-			<div class="text-xl font-semibold lg:text-2xl">Related Publications</div>
-			<PublicationListing items={item.fields.relatedPublications} />
-		</div>
-	{/if}
-
-	<div class="pt-6">
-		<a href="/videos">
-			<Button colors="green">View All Videos</Button>
-		</a>
-	</div>
+	</section>
 </article>
 
 <style>
