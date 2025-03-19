65f9990af262230a1cd0d71c94b9f3f77ea79431
brunosj <contact@landozone.net>
2025-03-19 09:38:13 +0100

    programme: topics and project cards improvement

--- src/lib/components/Programme/ProgrammeTopics.svelte
+++ src/lib/components/Programme/ProgrammeTopics.svelte
@@ -24,7 +24,7 @@
 		<h1 class="py-0 text-center text-2xl font-bold lg:py-6 lg:text-left lg:text-3xl">
 			{topic.fields.title}
 		</h1>
-		<div class="grid grid-cols-1 gap-y-3 py-6 lg:grid-cols-3">
+		<div class="grid auto-rows-auto grid-cols-1 gap-y-6 py-6 lg:grid-cols-3 lg:gap-x-10">
 			<div
 				class="rounded-md opacity-90 transition duration-300 ease-in-out group-hover:opacity-100"
 			>
@@ -32,10 +32,10 @@
 					loading="lazy"
 					src={topic.fields.image.fields.file.url}
 					alt={topic.fields.title}
-					class="rounded-md"
+					class="h-auto w-full rounded-md"
 				/>
 			</div>
-			<div class="col-span-2 pl-0 md:pl-10">
+			<div class="col-span-2 pl-0 md:pl-0">
 				<h2 class="text-center text-lg font-semibold leading-tight lg:text-left lg:text-xl">
 					{topic.fields.summary}
 				</h2>
@@ -51,7 +51,7 @@
 					<div class="flex py-3">
 						<button class="w-full lg:w-1/3" onclick={() => handleDescriptionMore(i)}>
 							<div
-								class="focus:shadow-outline border-blue-normal text-blue-normal hover:bg-blue-normal group flex items-center justify-around rounded-md border bg-white align-middle text-sm transition duration-300 ease-in-out hover:text-white focus:outline-none md:text-base"
+								class="focus:shadow-outline group flex items-center justify-around rounded-md border border-blue-normal bg-white align-middle text-sm text-blue-normal transition duration-300 ease-in-out hover:bg-blue-normal hover:text-white focus:outline-none md:text-base"
 							>
 								<div class="flex">
 									<div class="py-3 pl-3 font-bold leading-snug">Read more</div>
@@ -61,14 +61,18 @@
 					</div>
 				{/if}
 			</div>
-			<div class="col-span-2 col-start-2 pl-0 md:pl-10">
-				{#if topic.fields.projects}
-					<div>
-						<h2 class="pb-6 pt-3 text-lg font-bold lg:text-xl">Projects</h2>
-						<ProjectCard items={topic.fields.projects} />
-					</div>
-				{/if}
-			</div>
 		</div>
+
+		{#if topic.fields.projects}
+			<div class="mt-6 lg:mt-0 lg:pl-0">
+				<h2 class="pb-6 pt-3 text-lg font-bold lg:text-xl">Projects</h2>
+
+				<div class="grid-cols-2 gap-4 space-y-2 lg:grid lg:space-y-0">
+					{#each topic.fields.projects as project}
+						<ProjectCard item={project} />
+					{/each}
+				</div>
+			</div>
+		{/if}
 	</section>
 {/each}
--- src/lib/components/Programme/ProjectCard.svelte
+++ src/lib/components/Programme/ProjectCard.svelte
@@ -2,33 +2,32 @@
 	import type { Project } from '$lib/types/types';
 
 	interface Props {
-		items: Project[];
+		item: Project;
 	}
 
-	let { items }: Props = $props();
+	let { item }: Props = $props();
 </script>
 
-<div class="flex flex-wrap">
-	{#each items as item, i}
-		{@const image =
-			item.fields.thumbnailCdn?.length > 0
-				? item.fields.thumbnailCdn[0].secure_url
-				: item.fields.thumbnail.fields.file.url}
-		<div class="group w-full pb-2 pr-0 lg:w-1/2 lg:pr-5">
-			<a href={`/projects/${item.fields.slug}`}>
-				<div class="flex h-full overflow-hidden rounded-md border border-gray-300 bg-white">
-					<div
-						class="item-center flex w-1/4 flex-shrink-0 border-r-2 opacity-90 transition duration-300 ease-in-out group-hover:opacity-100"
-					>
-						<img loading="lazy" src={image} alt={item.fields.name} class="w-full object-cover" />
-					</div>
-					<div
-						class="group-hover:bg-blue-normal flex flex-grow items-center justify-center p-5 text-center text-base font-bold text-black transition duration-300 ease-in-out group-hover:text-green-light"
-					>
-						{item.fields.name}
-					</div>
-				</div>
-			</a>
+<div class="group w-full">
+	<a href={`/projects/${item.fields.slug}`} class="block h-full">
+		<div class="flex h-full overflow-hidden rounded-md border border-gray-300 bg-white">
+			<div
+				class="item-center flex w-1/4 flex-shrink-0 border-r-2 opacity-90 transition duration-300 ease-in-out group-hover:opacity-100"
+			>
+				<img
+					loading="lazy"
+					src={item.fields.thumbnailCdn?.length > 0
+						? item.fields.thumbnailCdn[0].secure_url
+						: item.fields.thumbnail.fields.file.url}
+					alt={item.fields.name}
+					class="aspect-square h-full w-full object-cover"
+				/>
+			</div>
+			<div
+				class="flex flex-grow items-center justify-center p-5 text-center text-base font-bold text-black transition duration-300 ease-in-out group-hover:bg-blue-normal group-hover:text-green-light"
+			>
+				{item.fields.name}
+			</div>
 		</div>
-	{/each}
+	</a>
 </div>
