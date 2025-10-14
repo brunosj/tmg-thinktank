b85b722d951c2c8cd1ffc34cbe605c26a7af536c
brunosj <contact@landozone.net>
2025-10-14 10:09:19 +0200

    DOI pages fix

--- src/routes/[doiNumber]/+page.svelte
+++ src/routes/[doiNumber]/+page.svelte
@@ -4,38 +4,41 @@
 	import SEO from '$components/SEO/SEO.svelte';
 
 	interface Props {
-		data: Publication;
+		data: {
+			item: Publication;
+		};
 	}
 
 	let { data }: Props = $props();
+	let item = $derived(data.item);
 </script>
 
 <SEO
-	title={data.fields.title}
-	description={data.fields.summary}
-	image={data.fields.thumbnail?.fields?.file?.url}
+	title={item.fields.title}
+	description={item.fields.summary}
+	image={item.fields.thumbnail?.fields?.file?.url}
 />
 <div class="layout pt-24 pb-12 lg:pt-40 lg:pb-16">
 	<div class="gril-cols-1 layout grid pb-10 lg:grid-cols-4">
-		{#if data.fields.thumbnail?.fields}
+		{#if item.fields.thumbnail?.fields}
 			<div
 				class="w-1/3 rounded-full px-2 pb-5 transition duration-150 ease-in-out group-hover:opacity-75 lg:w-full lg:pb-2"
 			>
-				{#if data.fields.pdf?.fields?.file?.url}
-					<a href={data.fields.pdf.fields.file.url}>
+				{#if item.fields.pdf?.fields?.file?.url}
+					<a href={item.fields.pdf.fields.file.url}>
 						<img
 							loading="lazy"
 							class=""
-							src={data.fields.thumbnail?.fields?.file?.url}
-							alt={data.fields.title}
+							src={item.fields.thumbnail?.fields?.file?.url}
+							alt={item.fields.title}
 						/>
 					</a>
 				{:else}
 					<img
 						loading="lazy"
 						class=""
-						src={data.fields.thumbnail?.fields?.file?.url}
-						alt={data.fields.title}
+						src={item.fields.thumbnail?.fields?.file?.url}
+						alt={item.fields.title}
 					/>
 				{/if}
 			</div>
@@ -45,37 +48,37 @@
 			<div class="space-y-6">
 				<div class="space-y-2">
 					<p class="text-blue-normal font-semibold">
-						{data.fields.category}
+						{item.fields.category}
 					</p>
 					<h2 class=" text-black">
-						{data.fields.title}
+						{item.fields.title}
 					</h2>
 					<p class="">
-						Written by <span class="font-semibold">{data.fields.author}</span>
+						Written by <span class="font-semibold">{item.fields.author}</span>
 					</p>
 				</div>
 				<p class="">
-					{data.fields.summary}
+					{item.fields.summary}
 				</p>
 				<p class="">
-					Full citation: <span class="italic">{data.fields.citation}</span>
+					Full citation: <span class="italic">{item.fields.citation}</span>
 				</p>
 				<div>
 					<p class="">
-						Published on <span class="font-semibold">{data.fields.publicationDate}</span>
+						Published on <span class="font-semibold">{item.fields.publicationDate}</span>
 					</p>
-					<a href={data.fields.doiUrl}>{data.fields.doiUrl}</a>
+					<a href={item.fields.doiUrl}>{item.fields.doiUrl}</a>
 				</div>
 				<div class="flex gap-2">
 					<div>
-						{#if data.fields.pdf?.fields?.file?.url}
-							<Button to={data.fields.pdf.fields.file.url} colors="green">Download</Button>
+						{#if item.fields.pdf?.fields?.file?.url}
+							<Button to={item.fields.pdf.fields.file.url} colors="green">Download</Button>
 						{/if}
 					</div>
-					{#if data.fields.additionalButtonFile?.fields}
+					{#if item.fields.additionalButtonFile?.fields}
 						<div>
-							<Button to={data.fields.additionalButtonFile.fields.file.url} colors="green">
-								{data.fields.additionalButtonText}
+							<Button to={item.fields.additionalButtonFile.fields.file.url} colors="green">
+								{item.fields.additionalButtonText}
 							</Button>
 						</div>
 					{/if}
